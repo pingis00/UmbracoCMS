abc9213f58bfbca5dbc199838d18430f80f26604
Andreas <pingis00@hotmail.com>
2024-10-07 08:00:54 +0200

    Forms finished

---
+++ UmbracoCMS_SubmissionTask/Models/FormCallbackModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UmbracoCMS_SubmissionTask.Models;
+
+public class FormCallbackModel
+{
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Phone { get; set; } = null!;
+    public string Service { get; set; } = null!;
+}
---
+++ UmbracoCMS_SubmissionTask/Models/FormQuestionModel.cs
@@ -0,0 +1,8 @@
+﻿namespace UmbracoCMS_SubmissionTask.Models;
+
+public class FormQuestionModel
+{
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Question { get; set; } = null!;
+}
